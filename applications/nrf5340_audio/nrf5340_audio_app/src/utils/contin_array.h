22174f3d9abc7dc3b62469d8b841975df9dfbef4
Alexander Svensen <alsv@nordicsemi.no>
2022-02-21 12:08:43 +0100

    applications: nrf5340_audio: Change doc for contin_array
    - Update documentation for contin_array.h
    
    Signed-off-by: Alexander Svensen <alsv@nordicsemi.no>

--- applications/nrf5340_audio/nrf5340_audio_app/src/utils/contin_array.h
+++ applications/nrf5340_audio/nrf5340_audio_app/src/utils/contin_array.h
@@ -11,8 +11,7 @@
 
 /** @brief. Creates a continuous array from a finite array
  * @param pcm_cont		Pointer to the destination array
- * @param pcm_cont_samples	Number of samples to put in pcm_cont. Must be as large
- *				or smaller than the size of the pcm_cont array
+ * @param pcm_cont_size	        Size of pcm_cont.
  * @param pcm_finite		Pointer to an array of samples/data
  * @param pcm_finite_size	Size of pcm_finite
  * @param finite_pos		Variable used interally. Must be set
